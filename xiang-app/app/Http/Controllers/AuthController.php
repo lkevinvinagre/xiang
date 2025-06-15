22ed65e3fbc771744841ccf1be3166b17c8d125f
lkevinvinagre <lkevinvinagre@hotmail.com>
2025-06-14 20:59:58 -0400

    api user login controller with jwt done

--- xiang-app/app/Http/Controllers/AuthController.php
+++ xiang-app/app/Http/Controllers/AuthController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -18,7 +18,18 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        return null;
+        $validator = Validator::make(($request->all()), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+        if(!$token = JWTAuth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Email or password is incorrect'], 401);
+        }
+        return $this->createNewToken($token);
+
     }
     public function logout(Request $request)
     {
@@ -41,7 +52,18 @@ class AuthController extends Controller
         ]);
         return response()->json([
             'message' => 'User registered successfully',
-            'user' => $user,
+            'user' => $user
         ], 201);
+    }
+
+    public function createNewToken($token)
+    {
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'expires_at' => JWTAuth::factory()->getTTL()*60,
+            'user' => JWTAuth::user()
+        ], 200);
     }
 }
