99400e69679560ac8673a5a6bdbdc2e0fafcd68c
lkevinvinagre <lkevinvinagre@hotmail.com>
2025-06-14 22:54:49 -0400

    Accounts validation with JWT working

--- xiang-app/app/Http/Controllers/AccountController.php
+++ xiang-app/app/Http/Controllers/AccountController.php
@@ -4,33 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Account;
+use Illuminate\Routing\Controller as BaseController;
 
-class AccountController extends Controller
+class AccountController extends BaseController
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     
     //List of accounts
     public function index()
     {
-        return Account::all();
+        return Account::where('user_id', auth()->guard('api')->user()->id)->get();
     }
 
     //Storage a new account
     public function store(Request $request)
     {
-        Account::create($request->all());
+        $data = $request->all();
+        $data['user_id'] = auth()->guard('api')->user()->id;
+        Account::create($data);
         return response()->json(['message' => 'Account created successfully'], 201);
     }
 
     //Display a specific account
-    public function show(string $id)
+    public function show(string $id )
     {
-        return Account::findOrFail($id);
+        return Account::where('id', $id)
+            ->where('user_id',auth()->guard('api')->user()->id)
+            ->firstOrFail();
     }
 
     //Update a specific account
     public function update(Request $request, string $id)
     {
-        $account = Account::findOrFail($id);
+        $account = Account::where('id', $id)->where('user_id',auth()->guard('api')->user()->id)->firstOrFail();
         $account->update($request->all());
         return $account;
     }
@@ -38,7 +48,7 @@ class AccountController extends Controller
     //Delete a specific account
     public function destroy(string $id)
     {
-        $account = Account::findOrFail($id);
+        $account = Account::where('id', $id)->where('user_id', auth()->guard('api')->user()->id)->firstOrFail();
         $account->delete();
         return response()->json(['message' => 'Account deleted successfully'], 200);
     }
