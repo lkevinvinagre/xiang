354b5fd7a20c771c7024de6df7d7e84f63815763
lkevinvinagre <lkevinvinagre@hotmail.com>
2025-06-17 00:32:24 -0400

    theme update

--- xiang-app/resources/views/fragments/listaccounts.blade.php
+++ xiang-app/resources/views/fragments/listaccounts.blade.php
@@ -1,20 +1,20 @@
 <section x-data="accountDashboard()" x-init="fetchAccounts()"  class="w-full flex flex-col items-start p-4">
-    <H1 class="p-4 text-6xl bold">Accounts:</H1>
+    <H1 class="p-4 text-6xl bold text-[#000000] dark:text-[#FFFFFF]">Accounts:</H1>@include('fragments.addaccount-modal')
     <table x-show="accounts.length > 0" class="table">
         <tr>
-            <td class="p-4">Title:</td>
-            <td class="p-4">URL:</td>
-            <td class="p-4">Username:</td>
+            <td class="p-4 text-[#000000] dark:text-[#FFFFFF]">Title:</td>
+            <td class="p-4 text-[#000000] dark:text-[#FFFFFF]">URL:</td>
+            <td class="p-4 text-[#000000] dark:text-[#FFFFFF]">Username:</td>
         </tr>
     <tbody>
         <template x-for="account in accounts" :key="account.id">
             <tr>
-                <td x-text="account.title" class="p-4"></td>
-                <td x-text="account.url" class="p-4"></td>
-                <td x-text="account.username" class="p-4"></td>
+                <td x-text="account.title" class="p-4 text-[#000000] dark:text-[#FFFFFF]"></td>
+                <td x-text="account.url" class="p-4 text-[#000000] dark:text-[#FFFFFF]"></td>
+                <td x-text="account.username" class="p-4 text-[#000000] dark:text-[#FFFFFF]"></td>
                 <td>
-                    <button @click="openModal(account.password)" class="show-pass text-white bg-blue-600 p-2">Show password</button>
-                    <button @click="deleteAccount(account.id)" class="text-red-500 p-2">Excluir</button>
+                    <button @click="openModal(account.password)" class="show-pass text-white bg-blue-600 dark:bg-red-600 p-2">Show password</button>
+                    <button @click="deleteAccount(account.id)" class="text-red-500 p-2">Delete</button>
                 </td>
             </tr>
         </template>
@@ -24,16 +24,16 @@
 <div x-show="modalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
     <div class="bg-white dark:bg-gray-900 rounded-lg p-6 w-full max-w-sm">
         <button @click="closeModal" class=" top-2 right-2 text-red-400">&times;</button>
-        <h2 x-show="!decryptedPassword" class="text-xl font-semibold text-center">Confirm your Password</h2>
-        <h2 x-show="decryptedPassword" class="text-xl font-semibold text-center">This is your Password</h2>
+        <h2 x-show="!decryptedPassword" class="text-xl font-semibold text-center text-[#000000] dark:text-[#FFFFFF]">Confirm your Password</h2>
+        <h2 x-show="decryptedPassword" class="text-xl font-semibold text-center text-[#000000] dark:text-[#FFFFFF]">This is your Password</h2>
         
         <form x-show="!decryptedPassword" @submit.prevent="decrypt">
             <input type="password" x-model="passwordInput" required class="w-full border rounded px-3 py-2 mb-2" />
             <div class="text-red-600 text-sm mb-2" x-text="error"></div>
-            <button type="submit" class="bg-blue-600 text-white w-full py-2 rounded">Confirm</button>
+            <button type="submit" class="bg-blue-600 dark:bg-red-600 text-white w-full py-2 rounded text-[#000000] dark:text-[#FFFFFF]">Confirm</button>
         </form>
 
-        <h3 x-show="decryptedPassword" class="text-lg text-center mt-4" x-text="decryptedPassword"></h3>
+        <h1 x-show="decryptedPassword" class="text-2xl font-semibold text-center mt-4 text-blue-600 dark:text-red-600" x-text="decryptedPassword"></h1>
     </div>
 </div>
 </section>
